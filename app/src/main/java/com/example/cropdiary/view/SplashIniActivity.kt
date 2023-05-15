f3703bfca004d8441c2a085262d64509f639c42d
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-05-15 15:02:37 -0500

    Update SplashIniActivity.kt

--- app/src/main/java/com/example/cropdiary/view/SplashIniActivity.kt
+++ app/src/main/java/com/example/cropdiary/view/SplashIniActivity.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import com.example.cropdiary.R
+import com.example.cropdiary.controller.UserController
 import com.example.cropdiary.view.Auth.AuthActivity
 import com.example.cropdiary.databinding.ActivitySplashIniBinding
+import com.example.cropdiary.util.utilities
 import com.example.cropdiary.view.main.MainActivity
+import com.example.cropdiary.view.user.CreateUserActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
@@ -16,46 +21,66 @@ import java.util.TimerTask
 
 class SplashIniActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashIniBinding
-
+    private lateinit var userController: UserController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashIniBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        userController = UserController(this)
+        /*val prefs=getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.clear()
+        prefs.apply()*/
         sesion()
     }
 
     private fun sesion() {
         val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
-        val email = prefs.getString("email", null)
-        val provider = prefs.getString("provider", null)
-
-        if (email!=null && provider!=null){
-            val timerTask = object : TimerTask() {
-                override fun run() {
-                    startActivity(
-                        Intent(this@SplashIniActivity, MainActivity::class.java)
-                    )
-                    finish()
-                }
+        val email = prefs.getString(getString(R.string.email_pref), null)
+        val provider = prefs.getString(getString(R.string.provider_pref), null)
+        /*object : CountDownTimer(2500, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                //
             }
 
-            val timer = Timer()
-            timer.schedule(timerTask, 2500)
-        }else{
-            val timerTask = object : TimerTask() {
-                override fun run() {
-                    startActivity(
-                        Intent(this@SplashIniActivity, AuthActivity::class.java).apply {
-                            putExtra("email", email)
-                            putExtra("provider", provider)
-                        }
-                    )
-                    finish()
-                }
+            override fun onFinish() {
+                checkUserData()
             }
 
-            val timer = Timer()
-            timer.schedule(timerTask, 2500)
+        }.start();*/
+
+    }
+
+    private fun checkUserData() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString(getString(R.string.email_pref), null)
+        val provider = prefs.getString(getString(R.string.provider_pref), null)
+        if (email != null && provider != null) {
+            if (utilities.networkConnection(this)) {
+                userController.getUser(email) { user ->
+                    if (user != null) {
+                        startActivity(
+                            Intent(this@SplashIniActivity, MainActivity::class.java).apply {
+                                putExtra("email", email)
+                                putExtra("provider", provider)
+                            }
+                        )
+                    } else {
+                        startActivity(
+                            Intent(this@SplashIniActivity, CreateUserActivity::class.java).apply {
+                                putExtra("email", email)
+                                putExtra("provider", provider)
+                            }
+                        )
+                    }
+                }
+                finish()
+            }
+
+        } else {
+            startActivity(
+                Intent(this@SplashIniActivity, AuthActivity::class.java)
+            )
+            finish()
         }
     }
 }
