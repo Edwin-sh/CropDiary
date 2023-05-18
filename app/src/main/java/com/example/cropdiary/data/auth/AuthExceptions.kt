a3f61f03a6c5b85e1e9c567b872b21563896ba6b
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-05-18 18:37:55 -0500

    Create AuthExceptions.kt

---
+++ app/src/main/java/com/example/cropdiary/data/auth/AuthExceptions.kt
@@ -0,0 +1,7 @@
+package com.example.cropdiary.data.auth
+
+object AuthExceptions {
+    const val ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND"
+    const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
+    const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
+}
