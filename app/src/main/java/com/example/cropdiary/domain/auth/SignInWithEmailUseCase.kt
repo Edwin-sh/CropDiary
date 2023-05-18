b57b22bf1cca4c5f3a162486b32986bfc3bd2812
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-05-18 18:40:57 -0500

    Create SignInWithEmailUseCase.kt

---
+++ app/src/main/java/com/example/cropdiary/domain/auth/SignInWithEmailUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.cropdiary.domain.auth
+
+import com.example.cropdiary.data.auth.AuthService
+import com.example.cropdiary.data.model.UserModel
+import com.google.firebase.auth.FirebaseUser
+
+class SignInWithEmailUseCase() {
+    private val service = AuthService()
+
+    suspend operator fun invoke(userModel: UserModel): Result<FirebaseUser?> {
+        return service.signInWithEmail(userModel)
+    }
+}
