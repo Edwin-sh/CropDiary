5b9e262b5ce416cf4cedffd7b194f16180c6284c
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-05-18 18:43:21 -0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/cropdiary/ui/view/main/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.cropdiary.ui.view.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cropdiary.core.FirebaseHelper
+import com.example.cropdiary.core.SharedPrefUserHelper
+import com.example.cropdiary.data.auth.ProviderType
+import com.example.cropdiary.data.repository.UserRepository
+import com.example.cropdiary.databinding.ActivityMainBinding
+import com.example.cropdiary.ui.view.Auth.AuthActivity
+import com.example.cropdiary.ui.viewmodel.AuthViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var email: String
+    private lateinit var provider: String
+    private lateinit var userRepository: UserRepository
+    private val authViewModel: AuthViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Setup
+
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        authViewModel.authResultModel.observe(this) {
+            SharedPrefUserHelper.clearPrefs(this)
+            if (it.isSuccess)
+                startActivity(Intent(this, AuthActivity::class.java))
+        }
+        val prefs = SharedPrefUserHelper.getUserPrefs(this)
+        email = prefs.email.toString()
+        provider = prefs.provider.toString()
+        setup(email, provider)
+        return super.onCreateView(name, context, attrs)
+
+    }
+
+    private fun setup(email: String, provider: String) {
+        binding.textViewEmail.text = email
+        binding.textViewProvider.text = provider
+        binding.button.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        authViewModel.signOut(
+            ProviderType.valueOf(provider),
+            FirebaseHelper.getGoogleSignInClient(this)
+        )
+    }
+}
