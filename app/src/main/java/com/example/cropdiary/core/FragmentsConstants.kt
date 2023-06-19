6166705e2587958b2e54e65c74b4a3380a99e7a8
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:36:18 -0500

    Update FragmentsConstants.kt

--- app/src/main/java/com/example/cropdiary/core/FragmentsConstants.kt
+++ app/src/main/java/com/example/cropdiary/core/FragmentsConstants.kt
@@ -1,6 +1,6 @@
 package com.example.cropdiary.core
 
 object FragmentsConstants {
-    const val FRAGMENT_AUTH = "AuthFragment"
+    const val FRAGMENT_SIGN_IN = "SignInFragment"
     const val FRAGMENT_RECOVERY_PASSWORD = "RecoveryPasswordFragment"
 }
