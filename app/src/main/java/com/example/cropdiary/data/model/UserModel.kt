d8a7f38bdf01583f48a7703ea11d54d7227d3bf4
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:42:43 -0500

    Update UserModel.kt

--- app/src/main/java/com/example/cropdiary/data/model/UserModel.kt
+++ app/src/main/java/com/example/cropdiary/data/model/UserModel.kt
@@ -4,16 +4,15 @@ import androidx.annotation.NonNull
 
 data class UserModel(
     @NonNull var idEmail: String,
-    @NonNull val password: String,
-    var photoProfile: String,
-    var name: String,
-    var lastname: String,
-    var phoneNumber: String,
-    var idCardNumber: String
-) {
+    var photoProfile: String? = null,
+    var name: String? = null,
+    var lastname: String? = null,
+    var phoneNumber: String? = null,
+    var idCardNumber: String? = null
+)/*{
     constructor(
         @NonNull idEmail: String,
         @NonNull password: String
     ) : this(idEmail, password, "", "", "", "", "")
-}
+}*/
 
