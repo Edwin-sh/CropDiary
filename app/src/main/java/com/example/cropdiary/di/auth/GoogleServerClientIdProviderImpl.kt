6610a49bc2fc12a279ac7a9e8225dacf99154359
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:39:29 -0500

    Google Server Cliente Class create

---
+++ app/src/main/java/com/example/cropdiary/di/auth/GoogleServerClientIdProviderImpl.kt
@@ -0,0 +1,7 @@
+package com.example.cropdiary.di.auth
+
+class GoogleServerClientIdProviderImpl : GoogleServerClientIdProvider {
+    override fun getGoogleServerClientId(): String {
+        return "318308638225-4t7bqu1b62v6b5sirb0mgcufk822c52j.apps.googleusercontent.com"
+    }
+}
