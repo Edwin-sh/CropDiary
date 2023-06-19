c9758874637ba7b64d82cc6e294ad80da1c6ee50
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:42:08 -0500

    Create FirestoreCollectionsHelper.kt

---
+++ app/src/main/java/com/example/cropdiary/core/FirestoreCollectionsHelper.kt
@@ -0,0 +1,7 @@
+package com.example.cropdiary.core
+
+object FirestoreCollectionsHelper {
+    val USERS_COLLECTION = "users_collection"
+    val DATA_USER_COLLECTION = "data_user_collection"
+    val DATA_INFO_DOCUMENT_KEY="data_user"
+}
