78edc96baa566c31b2047c0a73a9a3de02e20ce7
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:40:01 -0500

    Auth Use Case update

--- app/src/main/java/com/example/cropdiary/domain/auth/SignInWithEmailUseCase.kt
+++ app/src/main/java/com/example/cropdiary/domain/auth/SignInWithEmailUseCase.kt
@@ -1,12 +1,13 @@
 package com.example.cropdiary.domain.auth
 
 import com.example.cropdiary.data.auth.AuthService
+import com.example.cropdiary.data.model.FirebaseUserModel
 import com.example.cropdiary.data.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 import javax.inject.Inject
 
 class SignInWithEmailUseCase @Inject constructor (private val service :AuthService)  {
-    suspend operator fun invoke(userModel: UserModel): Result<FirebaseUser?> {
-        return service.signInWithEmail(userModel)
+    suspend operator fun invoke(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
+        return service.signInWithEmail(firebaseUserModel)
     }
 }
--- app/src/main/java/com/example/cropdiary/domain/auth/SignUpWithEmailUseCase.kt
+++ app/src/main/java/com/example/cropdiary/domain/auth/SignUpWithEmailUseCase.kt
@@ -1,12 +1,13 @@
 package com.example.cropdiary.domain.auth
 
 import com.example.cropdiary.data.auth.AuthService
+import com.example.cropdiary.data.model.FirebaseUserModel
 import com.example.cropdiary.data.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 import javax.inject.Inject
 
 class SignUpWithEmailUseCase @Inject constructor(private val service :AuthService) {
-    suspend operator fun invoke(userModel: UserModel): Result<FirebaseUser?> {
-        return service.signUpWithEmail(userModel)
+    suspend operator fun invoke(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
+        return service.signUpWithEmail(firebaseUserModel)
     }
 }
