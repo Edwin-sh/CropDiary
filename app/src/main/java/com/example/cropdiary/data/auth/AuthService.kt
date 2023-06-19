4b6dbc1f3d9a0db39109ebb1beb7d250905f9a24
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:42:25 -0500

    Update Auth Service

--- app/src/main/java/com/example/cropdiary/data/auth/AuthService.kt
+++ app/src/main/java/com/example/cropdiary/data/auth/AuthService.kt
@@ -1,70 +1,41 @@
 package com.example.cropdiary.data.auth
 
-import android.util.Log
-import com.example.cropdiary.R
-import com.example.cropdiary.data.model.UserModel
+import com.example.cropdiary.core.ExceptionsHelper.authException
+import com.example.cropdiary.core.TasksHelper
+import com.example.cropdiary.data.model.FirebaseUserModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class AuthService @Inject constructor(private val firebaseAuth: FirebaseAuth) : IAuthService {
-    override suspend fun signUpWithEmail(userModel: UserModel): Result<FirebaseUser?> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val result: FirebaseUser? =
-                    firebaseAuth.createUserWithEmailAndPassword(
-                        userModel.idEmail, userModel.password
-                    ).await().user
-                Result.success(result)
-            } catch (ex: Exception) {
-                Result.failure<FirebaseUser>(authException(ex))
-            }
-        }
+class AuthService @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val tasksHelper: TasksHelper
+) : IAuthService {
+    override suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
+        return tasksHelper.getAuthResult(
+            firebaseAuth.createUserWithEmailAndPassword(
+                firebaseUserModel.email, firebaseUserModel.password
+            )
+        )
     }
 
-    override suspend fun signInWithEmail(userModel: UserModel): Result<FirebaseUser?> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val result: FirebaseUser? =
-                    firebaseAuth.signInWithEmailAndPassword(
-                        userModel.idEmail, userModel.password
-                    )
-                        .await().user
-                Result.success(result)
-            } catch (ex: Exception) {
-                Result.failure<FirebaseUser>(authException(ex))
-            }
-        }
+    override suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
+        return tasksHelper.getAuthResult(
+            firebaseAuth.signInWithEmailAndPassword(
+                firebaseUserModel.email, firebaseUserModel.password
+            )
+        )
     }
 
     override suspend fun signInWithGoogle(idToken: String): Result<FirebaseUser?> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val credential = GoogleAuthProvider.getCredential(idToken, null)
-                val result: FirebaseUser? =
-                    firebaseAuth.signInWithCredential(credential).await().user
-                Result.success(result)
-            } catch (ex: Exception) {
-                Result.failure<FirebaseUser>(authException(ex))
-            }
-        }
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        return tasksHelper.getAuthResult(firebaseAuth.signInWithCredential(credential))
     }
 
     override suspend fun recoveryPassword(email: String): Result<Boolean> {
-        return withContext(Dispatchers.IO) {
-            try {
-                firebaseAuth.sendPasswordResetEmail(email).await()
-                Result.success(true)
-            } catch (ex: Exception) {
-                Result.failure(authException(ex))
-            }
-        }
+        return tasksHelper.getVoidResult(firebaseAuth.sendPasswordResetEmail(email))
     }
 
     override suspend fun signOut(
@@ -81,29 +52,6 @@ class AuthService @Inject constructor(private val firebaseAuth: FirebaseAuth) : 
             Result.success(true)
         } catch (ex: Exception) {
             Result.failure(authException(ex))
-        }
-    }
-
-    private fun authException(exception: Exception): Exception {
-        try {
-            throw exception!!
-        } catch (e: FirebaseAuthException) {
-            val errorMessage = when (e.errorCode) {
-                "ERROR_USER_NOT_FOUND" ->
-                    R.string.there_is_no_user_with_this_email.toString()
-                "ERROR_EMAIL_ALREADY_IN_USE" -> R.string.email_already_in_use.toString()
-                "ERROR_WRONG_PASSWORD" -> R.string.wrong_password.toString()
-                else -> {
-                    Log.w("Tag", "errorCode: ${e.errorCode}")
-                    "R.string"
-                }
-            }
-            return Exception(errorMessage, exception)
-        } catch (e: Exception) {
-            return Exception(
-                R.string.an_error_ocurred_authenticating_the_user.toString(),
-                exception
-            )
         }
     }
 }
--- app/src/main/java/com/example/cropdiary/data/auth/IAuthService.kt
+++ app/src/main/java/com/example/cropdiary/data/auth/IAuthService.kt
@@ -1,13 +1,14 @@
 package com.example.cropdiary.data.auth
 
+import com.example.cropdiary.data.model.FirebaseUserModel
 import com.example.cropdiary.data.model.UserModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseUser
 
 interface IAuthService {
-    suspend fun signUpWithEmail(userModel: UserModel): Result<FirebaseUser?>
+    suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?>
 
-    suspend fun signInWithEmail(userModel: UserModel): Result<FirebaseUser?>
+    suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?>
 
     suspend fun signInWithGoogle(idToken: String): Result<FirebaseUser?>
 
