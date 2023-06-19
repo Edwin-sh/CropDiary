4e7579b58e084ed6107961bc1734735fbffdd5fe
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-06-19 12:51:18 -0500

    Update UserViewModel.kt

--- app/src/main/java/com/example/cropdiary/ui/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/cropdiary/ui/viewmodel/UserViewModel.kt
@@ -4,19 +4,46 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cropdiary.data.model.UserModel
+import com.example.cropdiary.domain.user.CreateUserUseCase
 import com.example.cropdiary.domain.user.GetUserUseCase
+import com.example.cropdiary.domain.user.IsRegisteredUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel @Inject constructor(private val getUserUseCase :GetUserUseCase): ViewModel() {
-    val userModel = MutableLiveData<Result<UserModel?>>()
-    val userResultModel = MutableLiveData<Result<Boolean>?>()
-    fun getUser(email: String) {
+class UserViewModel @Inject constructor(
+    private val createUserUseCase: CreateUserUseCase,
+    private val isRegisteredUseCase: IsRegisteredUseCase,
+    private val getUserUseCase: GetUserUseCase
+) : ViewModel() {
+    val userDataModel = MutableLiveData<Result<UserModel?>>()
+    val userResultModel = MutableLiveData<Result<Boolean>>()
+    val _progressbar = MutableLiveData<Boolean>()
+    private val progressBarHelper=ProgressBarHelper(_progressbar)
+
+    fun isRegistered(email: String){
+        progressBarHelper.isLoading(true)
         viewModelScope.launch {
-            val result = getUserUseCase(email)
-            userModel.postValue(result)
+            val result= isRegisteredUseCase(email)
+            progressBarHelper.isLoading(false)
+            userResultModel.postValue(result)
+        }
+    }
+    fun createUser(userModel: UserModel) {
+        progressBarHelper.isLoading(true)
+        viewModelScope.launch {
+            val result= createUserUseCase(userModel)
+            progressBarHelper.isLoading(false)
+            userResultModel.postValue(result)
+        }
+    }
+    fun getUser(email: String) {
+        progressBarHelper.isLoading(true)
+        viewModelScope.launch {
+            val result= getUserUseCase(email)
+            progressBarHelper.isLoading(false)
+            userDataModel.postValue(result)
         }
     }
 }
