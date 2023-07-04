ee2c52bc3a8321e4d904327663ffb072999b0d7c
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-04 13:34:07 -0500

    Update UserDataCollection.kt

--- app/src/main/java/com/example/cropdiary/di/firebase/collections/UserDataCollection.kt
+++ app/src/main/java/com/example/cropdiary/di/firebase/collections/UserDataCollection.kt
@@ -1,13 +1,18 @@
 package com.example.cropdiary.di.firebase.collections
 
 import com.example.cropdiary.core.FirestoreCollectionsHelper
+import com.example.cropdiary.di.firebase.documents.UserDocumentReference
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import javax.inject.Inject
 
-class UserDataCollection @Inject constructor(private val usersCollection: UsersCollection) {
-    fun getUserDocument(userId: String): DocumentReference {
-        return usersCollection.collection.document(userId)
-            .collection(FirestoreCollectionsHelper.DATA_USER_COLLECTION)
-            .document(FirestoreCollectionsHelper.DATA_INFO_DOCUMENT_KEY)
+class UserDataCollection @Inject constructor(private val userDocumentReference: UserDocumentReference) {
+    fun getCollection(documentReference: DocumentReference):CollectionReference{
+        return documentReference.collection(FirestoreCollectionsHelper.DATA_USER_COLLECTION)
     }
+    fun getCollection():CollectionReference{
+        return userDocumentReference.document.collection(FirestoreCollectionsHelper.DATA_USER_COLLECTION)
+    }
+
+
 }
