7d5120b3ab1d81bbd85c4293584d84b25021bd70
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-04 13:27:21 -0500

    Update user repository

--- app/src/main/java/com/example/cropdiary/data/repository/IUserRepository.kt
+++ app/src/main/java/com/example/cropdiary/data/repository/IUserRepository.kt
@@ -1,10 +1,17 @@
 package com.example.cropdiary.data.repository
 
 import com.example.cropdiary.data.model.UserModel
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentSnapshot
 
 interface IUserRepository {
 
-    suspend fun createUser(userModel: UserModel): (Result<Boolean>)
+    suspend fun createUser(): Task<Void>
+    suspend fun addDataUser(userModel: UserModel): Task<Void>
+    suspend fun getUser(email: String): Task<DocumentSnapshot>
 
-    suspend fun getUser(email: String): (Result<UserModel?>)
+    suspend fun getDataUser(documentSnapshot: DocumentSnapshot): Task<DocumentSnapshot>
+    suspend fun getAppUser(): (Result<Any?>)
+
 }
--- app/src/main/java/com/example/cropdiary/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/cropdiary/data/repository/UserRepository.kt
@@ -1,35 +1,44 @@
 package com.example.cropdiary.data.repository
 
+import com.example.cropdiary.core.FirestoreCollectionsHelper
 import com.example.cropdiary.core.TasksHelper
+import com.example.cropdiary.core.documents.CropDataDocumentCore
 import com.example.cropdiary.data.model.UserModel
 import com.example.cropdiary.di.firebase.collections.UserDataCollection
 import com.example.cropdiary.di.firebase.collections.UsersCollection
+import com.example.cropdiary.di.firebase.documents.BasicDocument
+import com.example.cropdiary.di.firebase.documents.UserDataDocument
+import com.example.cropdiary.di.firebase.documents.UserDocumentReference
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
-    private val usersCollection: UsersCollection,
     private val userDataCollection: UserDataCollection,
+    private val userDataDocument: UserDataDocument,
+    private val userDocumentReference: UserDocumentReference,
+    private val basicDocument: BasicDocument,
+    private val usersCollection: UsersCollection,
     private val tasksHelper: TasksHelper
 ) : IUserRepository {
-
-    /*fun createUser() {
-        userRef.get().addOnSuccessListener { document ->
-            if (!document.exists()) {
-                userRef.set(hashMapOf<String, String>()).addOnSuccessListener {
-                    Log.d("UserModel Controller", "Se creó el documento")
-                }.addOnFailureListener { e ->
-                    Log.e("UserModel Controller", "Ocurrió un error al crear el documento", e)
-                }
-            } else {
-                Log.w("UserModel Controller", "Ya existía el documento")
-            }
-        }
-    }*/
-    override suspend fun createUser(userModel: UserModel): (Result<Boolean>) {
-        return tasksHelper.getVoidResult(userDataCollection.getUserDocument(userModel.idEmail).set(userModel))
+    override suspend fun createUser(): Task<Void> {
+        return basicDocument.emptyDocument(userDocumentReference.document)
+    }
+    override suspend fun addDataUser(userModel: UserModel): Task<Void> {
+        return userDataCollection.getCollection().document(FirestoreCollectionsHelper.DATA_INFO_DOCUMENT_KEY).set(userModel)
+    }
+    override suspend fun getUser(email:String): Task<DocumentSnapshot> {
+        return usersCollection.collection.document(email).get()
     }
 
-    override suspend fun getUser(email: String): (Result<UserModel?>) {
-        return tasksHelper.getDocumentResult(userDataCollection.getUserDocument(email).get())
+    override suspend fun getDataUser(documentSnapshot: DocumentSnapshot): Task<DocumentSnapshot> {
+        return userDataCollection.getCollection(documentSnapshot.reference).document(FirestoreCollectionsHelper.DATA_INFO_DOCUMENT_KEY).get()
+    }
+
+    override suspend fun getAppUser(): (Result<DocumentSnapshot?>){
+        return tasksHelper.getDocumentResult(userDataDocument.document.get())
     }
 }
