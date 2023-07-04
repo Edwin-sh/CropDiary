eaa0bf676dc0e7819bbd6af072309a26ef1ac08e
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-04 13:30:32 -0500

    Update Auth Service

--- app/src/main/java/com/example/cropdiary/data/auth/AuthService.kt
+++ app/src/main/java/com/example/cropdiary/data/auth/AuthService.kt
@@ -4,6 +4,8 @@ import com.example.cropdiary.core.ExceptionsHelper.authException
 import com.example.cropdiary.core.TasksHelper
 import com.example.cropdiary.data.model.FirebaseUserModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -13,45 +15,36 @@ class AuthService @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
     private val tasksHelper: TasksHelper
 ) : IAuthService {
-    override suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
-        return tasksHelper.getAuthResult(
-            firebaseAuth.createUserWithEmailAndPassword(
-                firebaseUserModel.email, firebaseUserModel.password
-            )
+    override suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Task<AuthResult> {
+        return firebaseAuth.createUserWithEmailAndPassword(
+            firebaseUserModel.email, firebaseUserModel.password
         )
     }
 
-    override suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?> {
-        return tasksHelper.getAuthResult(
-            firebaseAuth.signInWithEmailAndPassword(
+    override suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Task<AuthResult> {
+        return firebaseAuth.signInWithEmailAndPassword(
                 firebaseUserModel.email, firebaseUserModel.password
             )
-        )
     }
 
-    override suspend fun signInWithGoogle(idToken: String): Result<FirebaseUser?> {
+    override suspend fun signInWithGoogle(idToken: String): Task<AuthResult> {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        return tasksHelper.getAuthResult(firebaseAuth.signInWithCredential(credential))
+        return firebaseAuth.signInWithCredential(credential)
     }
 
-    override suspend fun recoveryPassword(email: String): Result<Boolean> {
-        return tasksHelper.getVoidResult(firebaseAuth.sendPasswordResetEmail(email))
+    override suspend fun recoveryPassword(email: String): Task<Void> {
+        return firebaseAuth.sendPasswordResetEmail(email)
     }
 
     override suspend fun signOut(
         provider: ProviderType,
         googleSignInClient: GoogleSignInClient
-    ): Result<Boolean> {
-        return try {
-            //Firebase Auth SignOut
+    ) {
+        //Firebase Auth SignOut
             firebaseAuth.signOut()
             if (provider == ProviderType.GOOGLE) {
                 //SignOut Google
                 googleSignInClient.signOut()
             }
-            Result.success(true)
-        } catch (ex: Exception) {
-            Result.failure(authException(ex))
-        }
     }
 }
--- app/src/main/java/com/example/cropdiary/data/auth/IAuthService.kt
+++ app/src/main/java/com/example/cropdiary/data/auth/IAuthService.kt
@@ -3,19 +3,21 @@ package com.example.cropdiary.data.auth
 import com.example.cropdiary.data.model.FirebaseUserModel
 import com.example.cropdiary.data.model.UserModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseUser
 
 interface IAuthService {
-    suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?>
+    suspend fun signUpWithEmail(firebaseUserModel: FirebaseUserModel): Task<AuthResult>
 
-    suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Result<FirebaseUser?>
+    suspend fun signInWithEmail(firebaseUserModel: FirebaseUserModel): Task<AuthResult>
 
-    suspend fun signInWithGoogle(idToken: String): Result<FirebaseUser?>
+    suspend fun signInWithGoogle(idToken: String): Task<AuthResult>
 
-    suspend fun recoveryPassword(email: String): Result<Boolean>
+    suspend fun recoveryPassword(email: String): Task<Void>
 
     suspend fun signOut(
         provider: ProviderType,
         googleSignInClient: GoogleSignInClient
-    ): Result<Boolean>
+    )
 }
