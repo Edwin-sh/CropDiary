65e4f81fc1755c1ca2447a710f65969489ec51ff
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-04 13:30:50 -0500

    Update FirestoreCollectionsHelper.kt

--- app/src/main/java/com/example/cropdiary/core/FirestoreCollectionsHelper.kt
+++ app/src/main/java/com/example/cropdiary/core/FirestoreCollectionsHelper.kt
@@ -2,6 +2,9 @@ package com.example.cropdiary.core
 
 object FirestoreCollectionsHelper {
     val USERS_COLLECTION = "users_collection"
+    val DATA_INFO_DOCUMENT_KEY="data_document"
     val DATA_USER_COLLECTION = "data_user_collection"
-    val DATA_INFO_DOCUMENT_KEY="data_user"
+    val CROPS_COLLECTION="crops_collection"
+    val DATA_CROP_COLLECTION = "data_crops_collection"
+    val WORKS_COLLECTION="works_collection"
 }
