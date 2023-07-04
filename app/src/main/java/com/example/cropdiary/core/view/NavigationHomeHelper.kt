43a6a0cdffecdcc0169ff8d04730159e72625697
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-04 13:34:39 -0500

    Create NavigationHomeHelper.kt

---
+++ app/src/main/java/com/example/cropdiary/core/view/NavigationHomeHelper.kt
@@ -0,0 +1,13 @@
+package com.example.cropdiary.core.view
+
+import android.app.Activity
+import android.content.Intent
+
+object NavigationHomeHelper {
+    /*fun showAddCropActivity(activity: Activity){
+        activity.startActivity(
+            Intent(activity, AddCropActivity::class.java)
+        )
+        activity.finish()
+    }*/
+}
